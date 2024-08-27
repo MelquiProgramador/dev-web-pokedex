0905609d81f0742e93078635a29bafee518093b7
Melqui Programador <mel38503@gmail.com>
2024-08-27 16:18:49 -0300

    Alimentação via JS usando PokeAPI das listas de pokemons.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,51 @@
- const url = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`;
+const limit = 10;
+const offset = 0;
+const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+function convertPokemonToLi(pokemon) {
+    return `
+    <li class="pokemon">
+        <div class="number">#001</div>
+            <h4 class="name">${pokemon.name}</h4>
+                <div class="detail">
+                    <div class="types">
+                        <div class="type">Grass</div>
+                        <div class="type">Poison</div>
+                    </div>
+                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Pokemon">
+                </div>
+    </li>
+    `
+}
+
+const pokemonList = document.getElementById("pokemonList");
+
 
 fetch(url)
-    .then(function (response){
-        return response.json();
+    .then((response) => response.json())
+    .then((listaPokemons) => listaPokemons.results)
+    .then((list) => {
+        for (let i = 0; i < list.length; i++){
+            const pokemon = convertPokemonToLi(list[i]);
+            pokemonList.innerHTML += pokemon;
+        }
     })
-    .then(function (responseBody){
-        console.log(responseBody.results);
-    })
-    .catch(function (error){
-        console.log(error);
-    })
+    .catch((error) => console.log(error));
+
+
+
+
+/* JAVACRIPT 3 MANEIRAS DE FAZER FUNÇÃO */
+
+/* 1 */
+function soma(a, b) {
+    return a + b;
+}
+
+/* 2 */
+const soma1 = (a, b) => a + b;
+
+/* 3 */
+const soma2 = (a, b) => {
+    return a + b;
+}
