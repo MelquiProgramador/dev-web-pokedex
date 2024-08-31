968aff7288d549cd5adf770a8b21ee1835cc92a0
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-08-31 19:59:08 -0300

    Add: new style

--- js/main.js
+++ js/main.js
@@ -3,36 +3,38 @@ const offset = 0;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
 function convertPokemonToLi(pokemon) {
+
+    let pokemonIndex = pokemon.url.split("/")
+
     return `
     <li class="pokemon">
-        <div class="number">#${JSON.stringify(pokemon.order)}</div>
-            <h4 class="name">${pokemon.name}</h4>
-                <div class="detail">
-                    <div class="types">
-                        <div class="type">Grass</div>
-                        <div class="type">Poison</div>
-                    </div>
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="Pokemon">
+
+             <div class="img-container">
+                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonIndex[pokemonIndex.length - 2]}.svg" alt="Pokemon: ${pokemon.name}">
+                <p class="number">#${pokemonIndex[pokemonIndex.length - 2]}</p>
+             </div>
+            <div class="detail">
+                <h4 class="name">${pokemon.name}</h4>
+                <div class="types">
+
                 </div>
+            </div>
     </li>
     `
 }
 
 const pokemonList = document.getElementById("pokemonList");
 
-
 fetch(url)
-    .then((response) => response.json())
+    .then((response) => (response.json()))
     .then((listaPokemons) => listaPokemons.results)
     .then((list) => {
-        for (let i = 0; i < list.length; i++){
-            const pokemon = convertPokemonToLi(list[i]);
+        list.forEach((element) => {
+            const pokemon = convertPokemonToLi(element);
             pokemonList.innerHTML += pokemon;
-        }
+        });
     })
     .catch((error) => console.log(error));
-
-
 
 
 /* JAVACRIPT 3 MANEIRAS DE FAZER FUNÇÃO */
