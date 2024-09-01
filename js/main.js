2a627c54d2cfbefbd853c1c606987078779e99e6
Gabriel Vergílio <125078273+GabrielVSP@users.noreply.github.com>
2024-08-31 22:42:48 -0300

    Fixed: types of pokemon are now displayed

--- js/main.js
+++ js/main.js
@@ -1,41 +1,57 @@
-const limit = 10;
+const limit = 12;
 const offset = 0;
 const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+const pokemonList = document.getElementById("pokemonList");
 
 function convertPokemonToLi(pokemon) {
 
-    let pokemonIndex = pokemon.url.split("/")
+    const types = document.createElement('div')
+    types.classList.add('types')
 
+    const typeSearch = pokemon.types.map((typeObj) => types.innerHTML += `<div class='${typeObj.type.name} type'>${typeObj.type.name}</div>`)
+    
     return `
     <li class="pokemon">
 
              <div class="img-container">
-                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemonIndex[pokemonIndex.length - 2]}.svg" alt="Pokemon: ${pokemon.name}">
-                <p class="number">#${pokemonIndex[pokemonIndex.length - 2]}</p>
+                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="Pokemon: ${pokemon.name}">
+                <p class="number">#${pokemon.id}</p>
              </div>
             <div class="detail">
                 <h4 class="name">${pokemon.name}</h4>
                 <div class="types">
-
+                    ${types.innerHTML}
                 </div>
             </div>
     </li>
     `
 }
 
-const pokemonList = document.getElementById("pokemonList");
+async function LoadPokemon() {
+    
+    try {
 
-fetch(url)
-    .then((response) => (response.json()))
-    .then((listaPokemons) => listaPokemons.results)
-    .then((list) => {
-        list.forEach((element) => {
-            const pokemon = convertPokemonToLi(element);
-            pokemonList.innerHTML += pokemon;
-        });
-    })
-    .catch((error) => console.log(error));
+        const res = await fetch(url)
+        const {results: data} = await res.json()
 
+        const promisses = data.map((result) => fetch(result.url))
+        const responses = await Promise.allSettled(promisses)
+        const ready = responses.filter(res => res.status === 'fulfilled')
+        const pokePromisses = ready.map((uri) => uri.value.json())
+        const pokemon = await Promise.all(pokePromisses)
+
+        pokemon.forEach((poke) => {
+            let pokeCard = convertPokemonToLi(poke)
+            pokemonList.innerHTML += pokeCard
+        })
+
+    }catch(e) {
+        console.error(e)
+    }
+
+}
+
+LoadPokemon()
 
 /* JAVACRIPT 3 MANEIRAS DE FAZER FUNÇÃO */
 
